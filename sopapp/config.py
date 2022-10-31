9e853298f73407af5128c18f45f9d7598567f02c
omprakash2469 <oprajapati249@gmail.com>
2022-11-01 01:50:54 +0530

    development and production setup

--- sopapp/config.py
+++ sopapp/config.py
@@ -1,16 +1,16 @@
 # --------------- Development Config ---------------
-class Config(object):
-    DEBUG = True
-    DEVELOPMENT = True
-    SECRET_KEY = "This is my secret key"
-    SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost/soplaptops"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# class Config(object):
+#     DEBUG = True
+#     DEVELOPMENT = True
+#     SECRET_KEY = "This is my secret key"
+#     SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost/soplaptops"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 # --------------- Production Config ---------------
-# class Config(object):
-#     DEBUG = False
-#     DEVELOPMENT = False
-#     SECRET_KEY = "this-is-my-super-secret-key"
-#     SQLALCHEMY_DATABASE_URI = "sqlite:///soplaptops.db"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config(object):
+    DEBUG = False
+    DEVELOPMENT = False
+    SECRET_KEY = "afijoi23jooHOIFOEIKJLDFJLSDJFaoefhaognowe2342523413joijfao2342"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///soplaptops.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
