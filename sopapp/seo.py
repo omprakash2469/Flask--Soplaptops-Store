77b39fc60c592b9f93f481edf759bdd172aa5588
omprakash2469 <oprajapati249@gmail.com>
2022-11-27 16:37:25 +0530

    added seo

---
+++ sopapp/seo.py
@@ -0,0 +1,66 @@
+brand_name = "SOP Laptops"
+primary_keyword = "Best Laptops in Pune"
+meta = {
+    "home": {
+        "title": f"{brand_name} || {primary_keyword}",
+        "desc": "We provide best laptops in pune with great deals. Laptops for various usecase like office, students, coding and programming and many more.",
+        "canonical": ""
+    },
+    "about": {
+        "title": "About Us || " + brand_name,
+        "desc": "SOP Laptops provides best deals on laptops in pune with free home delivery",
+        "canonical": ""
+    },
+    "blogs": {
+        "title": "Our Blogs || " + brand_name,
+        "desc": "In Our Blogs, we guide you through the best deals on laptops to buy for your different use cases",
+        "canonical": ""
+    },
+    "single_blog": {
+        "title": "",
+        "desc": "",
+        "canonical": ""
+    },
+    "category": {
+        "title": "",
+        "desc": "",
+        "canonical": ""
+    },
+    "product": {
+        "title": "",
+        "desc": "Best Laptop in pune",
+        "canonical": ""
+    },
+    "contact": {
+        "title": "Contact Us || " + brand_name,
+        "desc": "Feel Free to contact and we will provide best possible service for you",
+        "canonical": ""
+    },
+    "login": {
+        "title": "Login || " + brand_name,
+        "desc": "Please Login to complete your purchase",
+        "canonical": ""
+    },
+    "signup": {
+        "title": "Sign Up || " + brand_name,
+        "desc": "Please Signup to complete your purchase",
+        "canonical": ""
+    },
+    "privacy-policy": {
+        "title": "Privacy Policies || " + brand_name,
+        "desc": "Read our privacy policies for your better understanding",
+        "canonical": ""
+    },
+    "user": {
+        "title": "",
+        "desc": "current loggedin user soplaptops",
+    },
+    "orderConfirm": {
+        "title": "Your Order has been placed",
+        "desc": "Your order is confirmed",
+    },
+    "checkout": {
+        "title": "Checkout | " + brand_name,
+        "desc": "Purchase your laptop for college use",
+    }
+}
